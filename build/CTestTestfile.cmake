49f008eb562e1fe2c430d9a25380d56f47bf047f
lakshmipranathiMaddirala <lmaddira@andrew.cmu.edu>
2020-11-18 16:53:56 -0500

    latest

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lakshmi/MPC
-# Build directory: /home/lakshmi/MPC/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
