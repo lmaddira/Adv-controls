96e368c172f587237579868b49d8148377f7dd89
nobbybanerjee <73766479+nobbybanerjee@users.noreply.github.com>
2020-12-05 13:36:06 -0500

    Add files via upload

---
+++ 11_29_2020_12_09_I2C_Master/mode.h
@@ -0,0 +1,15 @@
+enum FUNCTION_MODE
+{
+  IDLE,
+} function_mode = IDLE;
+
+enum MOTION_MODE
+{
+  STANDBY,
+  FORWARD,
+  BACKWARD,
+  TURNLEFT,
+  TURNRIGHT,
+  STOP,
+  START,
+} motion_mode = START;
